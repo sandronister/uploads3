633b827d687658383ca51017424f735f52bd0a64
Julio Sandroni <sandronister@gmail.com>
2023-10-21 11:02:29 -0300

    update config

--- configs/config.go
+++ configs/config.go
@@ -3,16 +3,17 @@ package configs
 import "github.com/spf13/viper"
 
 type awsConf struct {
-	Credentials string `mapstructure:"AWS_CREDENTIALS"`
-	Password    string `mapstructure:"AWS_PASSWORD"`
-	Bucket      string `mapstructure:"BUCKET_AWS"`
+	AcessKey  string `mapstructure:"ACESS_KEY"`
+	SecretKey string `mapstructure:"SECRET_KEY"`
+	Bucket    string `mapstructure:"BUCKET"`
 }
 
 func GetConfig(path string) (*awsConf, error) {
 	var cfg *awsConf
-	viper.SetConfigName("APP_S3")
+	viper.SetConfigName("app_config")
 	viper.SetConfigType("env")
 	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
 	viper.AutomaticEnv()
 	err := viper.ReadInConfig()
 	if err != nil {
